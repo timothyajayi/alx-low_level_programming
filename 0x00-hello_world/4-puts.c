86bd736e0379f99ad39c9039dda2a57950087114
timothyajayi <timothyajayi293@gmail.com>
2023-07-14 08:40:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-#include <stdio.h> intmain(){puts("Programming is like building a multilingual puzzle");return 0;}
+#include <stdio.h> /** *main entry*/ int main(void){puts("Programming is like building a multilingual puzzle");return 0;}
