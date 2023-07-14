88106a8f9507314117a58b5a0ecd4fe3cf257b04
timothyajayi <timothyajayi293@gmail.com>
2023-07-14 09:11:03 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,5 +2,5 @@
 #include <stdio.h>
 main()
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n")
-;}
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+;return 0;}
