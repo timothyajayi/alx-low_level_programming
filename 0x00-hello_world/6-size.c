49684f6fb1fbabf8714f2ffd8f5614a98773ac48
timothyajayi <timothyajayi293@gmail.com>
2023-07-15 14:01:32 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	printf(// C program to find the size of all data types using the sizeof operator
+#include <stdio.h>
+
+int main() {
+
+    // Calculate the size of short int data type
+    printf("The size of short int: %ld bytes\n", sizeof(short int));
+
+    // Calculate the size of unsigned short int data type
+    printf("The size of unsigned short int: %ld bytes\n", sizeof(unsigned short int));
+
+    // Calculate the size of unsigned int data type
+    printf("The size of unsigned int: %ld bytes\n", sizeof(unsigned int));
+
+    // Calculate the size of int data type
+    printf("The size of int: %ld bytes\n", sizeof(int));
+
+    // Calculate the size of long int data type
+    printf("The size of long int: %ld bytes\n", sizeof(long int));
+
+    // Calculate the size of unsigned long int data type
+    printf("The size of unsigned long int: %ld bytes\n", sizeof(unsigned long int));
+
+    // Calculate the size of long long int data type
+    printf("The size of long long int: %ld bytes\n", sizeof(long long int));
+
+    // Calculate the size of unsigned long long int data type
+    printf("The size of unsigned long long int: %ld bytes\n", sizeof(unsigned long long int));
+
+    // Calculate the size of signed char data type
+    printf("The size of signed char: %ld byte\n", sizeof(signed char));
+
+    // Calculate the size of unsigned char data type
+    printf("The size of unsigned char: %ld byte\n", sizeof(unsigned char));
+
+    // Calculate the size of char data type
+    printf("The size of char: %ld byte\n", sizeof(char));
+
+    // Calculate the size of float data type
+    printf("The size of float: %ld bytes\n", sizeof(float));
+
+    // Calculate the size of double data type
+    printf("The size of double: %ld bytes\n", sizeof(double));
+
+
+    printf("The size of long double: %ld bytes\n", sizeof(long double));
+
+    return 0;
+}
