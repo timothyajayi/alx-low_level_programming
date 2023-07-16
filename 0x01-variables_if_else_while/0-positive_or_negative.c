f0127a6ef1767a778d4d63c3c748e655c22a15fb
timothyajayi <timothyajayi293@gmail.com>
2023-07-16 10:49:04 -0700

    positive,negative and zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main-Entry point
+ * main - determination of positive,negative and zero
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
