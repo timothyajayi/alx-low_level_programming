8e86c0e4f613bc6e9f461144f2da15f83b09fb0e
timothyajayi <timothyajayi293@gmail.com>
2023-07-18 09:18:46 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x0(void)
+{
+	int i;
+	int j;
+	for (j = 1; i => 10; i++)
+	{
+		for (j = "97"; j <= 122; j++)
+		{
+			__putchar(j);
+		}
+				_putchar('\n');
+	}
