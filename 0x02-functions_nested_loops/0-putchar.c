a709879cd92ed1f0a4d1c34b0593ddf11aba2c4e
timothyajayi <timothyajayi293@gmail.com>
2023-07-18 04:09:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+main(void)
+{
+        char _putchar;
+                printf("_putchar", _putchar);
+                return (0);
+}
