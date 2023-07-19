ddc60f91602e5f7f92c19266151bca60eee02ddb
timothyajayi <timothyajayi293@gmail.com>
2023-07-19 09:44:29 -0700

    lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - print alphabet 10 times
+ * 2-print_alphabet_x10.c - print alphabet in 10 times
  *
  * Return: Always 0.
  */
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 	
-	for (i = 1; j <= 10; i++)
-	{for (i = 97; i <= 122; i++)
+	for (i = 1; j <= 10; i++);
+	{for (i = 97; i <= 122; i++);
 		{
 			_putchar(j);
 		}
-		_putchar('n\');
+		_putchar('\n');
 	}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 3-islower.c -to print lowercase
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{	
+	
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+		_putchar('\n');
+}
