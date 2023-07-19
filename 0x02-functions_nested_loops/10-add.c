5a700e1bf1b78b0499f883baa27ec5492cab2975
timothyajayi <timothyajayi293@gmail.com>
2023-07-19 15:47:10 -0700

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "mai.h"
+
+/**
+ * add -  sum between two numbers
+ *
+ * @a : adding two integers
+ *
+ * @b : number two
+ * Return : sum
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+
+	__putchar('\n')
+}
+
+
