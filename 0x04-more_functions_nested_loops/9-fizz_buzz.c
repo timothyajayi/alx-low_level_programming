f35beaede878c21a17390df5875d68e4b2bbbaa5
timothyajayi <timothyajayi293@gmail.com>
2023-07-20 09:31:19 -0700

    0x04-more_functions_nested_loop

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print multiples of three, five and both.
+ * Return: 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 100; n++)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
