1ee9983c323c9a904539f80fb028624e387029d3
timothyajayi <timothyajayi293@gmail.com>
2023-07-24 03:15:39 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - to write a function that returns the length of a string
+ * @s: s as the input char
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
+}
