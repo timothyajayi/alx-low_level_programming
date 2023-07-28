76333d0729bb7041d2682d1afd01c6baecb07ce0
timothyajayi <timothyajayi293@gmail.com>
2023-07-28 08:15:53 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * infinite_add - function that adds two number
+ * @n1: first number
+ * @n2: second digit
+ * @r: char
+ * @size_r: size
+ * Return: to return 0
+ **/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+	for (i = 1; i <= n2; i++)
+
+	n1++;
+
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
