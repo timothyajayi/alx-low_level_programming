bb1562d936f9e116baf81496f16a9a0d4f53330a
timothyajayi <timothyajayi293@gmail.com>
2023-08-04 07:49:16 -0700

    0-isupper.o

---
+++ 0x09-static_libraries/islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - lowercase.
+ * @c: 3-islower.c
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+		_putchar('\n');
+}
