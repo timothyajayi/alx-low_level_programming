61db8300fd1d4b231ceceb1091955d5c3f883940
timothyajayi <timothyajayi293@gmail.com>
2023-08-05 09:34:13 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name + \n
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+        printf("%d", argc);
+        printf("argv[0] %s\n", argv[0]);
+        return (0);
+}
