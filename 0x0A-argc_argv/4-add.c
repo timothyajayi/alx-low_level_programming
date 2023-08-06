3f57323de7bc70b4bd9331e68433216f1d9af93f
timothyajayi <timothyajayi293@gmail.com>
2023-08-06 14:04:07 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,16 +11,16 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc < 1)
-		return (0);
+	return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+	if (!atoi(argv[i]))
+	{
+	printf("%s\n", "Error");
+	return (1);
+	}
+	sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 
