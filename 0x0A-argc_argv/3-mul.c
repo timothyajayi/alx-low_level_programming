51b17f1b9dde1c0100560a4c9e3adc5c5df08f6c
timothyajayi <timothyajayi293@gmail.com>
2023-08-06 13:39:00 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * main - Entry Point
- * @mul: is a function that converts a string into an int
- * @a: arguments
- * @b: array pointing to arguments
+ * atoi: is a function that converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
  * Return: 0
  */
-int mul(int a, int b)
+int main(int argc, char *argv[])
 {
-	return (a * b);
+	int i;
+	int result = 1;
+
+	if (argc != 3)
+	{
+	printf("%s\n", "Error");
+	return (1);
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	result = result * atoi(argv[i]);
+	printf("argv [%d] = %s\n", i,  argv[i]);
+	}
+
+	printf("The multiples of result is %d\n", result);
+
+
+	return (0);
 }
